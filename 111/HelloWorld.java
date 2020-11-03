0c667a6c2342d9b297b6526645676552870a4182
pudding <pudding@itcast.cn>
2020-11-03 12:27:02 +0800

    System.out.println("nihao");

--- 111/HelloWorld.java
+++ 111/HelloWorld.java
@@ -1,7 +1,12 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("nihao");
 	}
+
 }
 
 
