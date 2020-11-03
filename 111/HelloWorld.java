aa145ffed9c176f825138e662341628d60f16e41
pudding <pudding@itcast.cn>
2020-11-03 12:35:08 +0800

    System.out.println("nihaoma");

--- 111/HelloWorld.java
+++ 111/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("nihao");
-		System.out.println("howareyou");
+		System.out.println("nihaoma");
 	}
 
 }
